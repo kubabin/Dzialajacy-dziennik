c36673e62e8d86664b755eb72e67830f5ef482ab
kubabin <58891993+kubabin@users.noreply.github.com>
2022-05-30 18:41:18 +0200

    add functions to data parser

--- src/app/data-parser/data-parser.component.js
+++ src/app/data-parser/data-parser.component.js
@@ -1,14 +1,51 @@
 class Data{
-  constructor(){
-    this.accounts = require("./data/accounts.json");
-    this.persons = require("./data/persons.json");
-  }
-  //function read()
+    static accounts = require("./data/accounts.json");
+    static persons = require("./data/persons.json");
 }
-class Accounts{
+class AccountsService{
   constructor(){}
-  static check(login, password){
-
+  static getAccounts(login, password){
+    Data.accounts.forEach(element => {
+      if(element.login == login && element.password == sha256(password)){
+        document.cookie = "login=" + login;
+        document.cookie = "password=" + sha256(password);
+        return element.students;
+      }
+    });
+  }
+}
+class DataService{
+  constructor(){}
+  static getStudents(studentClass){
+    return Data.persons.students;
+  }
+  static getTeachers(){
+    return Data.persons.teachers;
+  }
+  static getParents(){
+    return Data.persons.parents;
+  }
+  static addStudent(student){
+    Data.persons.students.push(student);
+  }
+  static addTeacher(teacher){
+    Data.persons.teachers.push(teacher);
+  }
+  static addParent(parent){
+    Data.persons.parents.push(parent);
+  }
+  static getStudent(id){
+    return Data.persons.students.find(student => student.id == id);
+  }
+  static getTeacher(id){
+    return Data.persons.teachers.find(teacher => teacher.id == id);
+  }
+  static getParent(id){
+    return Data.persons.parents.find(parent => parent.id == id);
   }
 
+}
+
+function sha256(input){
+  return crypto.createHash('sha256').update(JSON.stringify(input)).digest('hex')
 }
